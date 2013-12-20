079db7edab29b24749a81cb50d5167598a2a77a4
jwyuana <jwyuan2004@gmail.com>
2013-12-20 16:22:33 -0600

    dp

--- src/POJ1163.java
+++ src/POJ1163.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
 
 public class POJ1163 {
 
+	public static void main(String args[]){
+		Scanner cin = new Scanner(System.in);
+		int N = cin.nextInt();
+		int[][] records = new int[N][N];
+		
+		for(int i=0; i<N; i++)
+			for(int j=0; j<=i; j++){
+				records[i][j] = cin.nextInt();
+			}
+		
+		for(int i=N-2; i>=0; i--)
+			for(int j=0; j<=i; j++){
+				records[i][j] += Math.max(records[i+1][j], records[i+1][j+1]);
+			}
+		
+		System.out.println(records[0][0]);
+	}
 }
